886f05e92e945537f627bdfc38153d490e049505
Isaac David <orpheezt@outlook.com>
2026-04-26 21:39:22 -0500

    Minor changes

--- image.py
+++ image.py
@@ -2,21 +2,18 @@ import modal
 
 image = (
     modal.Image.from_registry("nvidia/cuda:13.2.1-devel-ubuntu24.04", add_python="3.14")
-    .apt_install(["libxcb1", "libgl1", "libglib2.0-0"])
     .uv_pip_install(
         "torch>=2.11",
         "torchvision",
-        "ultralytics",
         "pillow",
         "pandas",
         "matplotlib",
-        "kagglehub",
         "ipykernel",
         "ipywidgets",
     )
 )
 
-app = modal.App("Taller_9_IV")
+app = modal.App("Taller_10_IV")
 
 
 @app.function(image=image)
