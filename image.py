1edf378f498d005ffd107e1f19c2f6f2848927f4
Isaac David <orpheezt@outlook.com>
2026-04-26 22:55:06 -0500

    Fix image

--- image.py
+++ image.py
@@ -1,16 +1,16 @@
 import modal
 
-image = (
-    modal.Image.from_registry("nvidia/cuda:13.2.1-devel-ubuntu24.04", add_python="3.14")
-    .uv_pip_install(
-        "torch>=2.11",
-        "torchvision",
-        "pillow",
-        "pandas",
-        "matplotlib",
-        "ipykernel",
-        "ipywidgets",
-    )
+image = modal.Image.from_registry(
+    "nvidia/cuda:13.2.1-devel-ubuntu24.04", add_python="3.14"
+).uv_pip_install(
+    "grain",
+    "jax[cuda13]",
+    "keras",
+    "pillow",
+    "pandas",
+    "matplotlib",
+    "ipykernel",
+    "ipywidgets",
 )
 
 app = modal.App("Taller_10_IV")
